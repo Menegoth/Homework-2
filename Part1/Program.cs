31a42b14643fa203a2afefc214c81fe575a5fc94
Antonio <antonioareas@me.com>
2020-09-01 18:30:47 -0700

    Add project files.

---
+++ Part1/Program.cs
@@ -0,0 +1,56 @@
+﻿/// Week No. 2	Homework No. 1
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  September 1st, 2020
+
+using System;
+using System.Linq;
+
+namespace Part1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declare variables
+            string firstLetter;
+            string fullName;
+
+            //declare string variables
+            string pigLatin = "ay";
+            string first = "antonio";
+            string last = "monteiro";
+
+            //fix first name
+            //start with getting firstletter
+            firstLetter = first.Substring(0, 1);
+
+            //remove first letter from name and add it to the end
+            first = first.Substring(1);
+            first = first + firstLetter + pigLatin;
+
+            //capitalize it
+            first = first.Substring(0, 1).ToUpper() + first.Substring(1);
+
+            //do the same for last name
+            //start with getting firstletter
+            firstLetter = last.Substring(0, 1);
+
+            //remove first letter from name and add it to the end
+            last = last.Substring(1);
+            last = last + firstLetter + pigLatin;
+
+            //capitalize it
+            last = last.Substring(0, 1).ToUpper() + last.Substring(1);
+
+            //combine both names
+            fullName = first + " " + last;
+            Console.WriteLine(fullName);
+
+            //pause console
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Part2/Program.cs
@@ -0,0 +1,35 @@
+﻿/// Week No. 2	Homework No. 2
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  September 1st, 2020
+
+using System;
+using System.Reflection;
+
+namespace Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //declare variables
+            double degreesF, degreesC;
+
+            //ask user for Farenhenti
+            Console.WriteLine("Enter a temperature in Farenheit: ");
+            degreesF = double.Parse(Console.ReadLine());
+
+            //convert and round to one decimal point
+            degreesC = 5 * (degreesF - 32) / 9;
+            degreesC = Math.Round(degreesC, 1);
+
+            //inform the user
+            Console.WriteLine(degreesC);
+
+            //pause console
+            Console.ReadLine();
+
+        }
+    }
+}
